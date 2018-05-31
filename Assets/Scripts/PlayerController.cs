6345c0f3f0f35ca10270c053283c185f37d3e92a
LAPTOP-Q6FQDGEF\chetobros <chetobros@gmail.com>
2018-05-31 13:03:40 -0500

    Starting project
    Incluye el script inicial del personaje 2d plataformero

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[Header("Player properties")]
+	public float speed;
+	public float jumpForce;
+	[Header("Extra player properties")]
+	public float extraYCollide;
+	[Header("Gravity modifiers")]
+	public float jumpMultiplier;
+	public float gravityIncreaser;
+	[Header("Shoot properties")]
+	[SerializeField]
+	private Transform bulletSpawnPoint;
+	[SerializeField]
+	private Pool pool;
+
+	private bool jumped;
+	private Rigidbody player;
+	private Collider collider;
+
+	void Start(){
+		player = GetComponent<Rigidbody> ();
+		collider = GetComponent<BoxCollider> ();
+	}
+
+	void Update(){
+		//Reset values with ground
+		if (IsGrounded ()) {
+			jumped = false;
+		}
+		//Input values
+		float horizontal = Input.GetAxis ("Horizontal"); 
+		bool vertical = Input.GetButtonDown ("Jump");
+		//Horizontal move
+		if (horizontal!= 0) {
+			player.velocity = new Vector3 (speed * horizontal * Time.deltaTime, player.velocity.y, 0f);
+		}
+		//Jump
+		if (vertical && !jumped) {
+			player.AddForce (Vector3.up * jumpForce);
+			jumped = true;
+		}
+		//Fix jump
+		if (player.velocity.y > 0) {
+			player.velocity += Vector3.up * Physics.gravity.y * (jumpMultiplier - 1) * Time.deltaTime;
+		}
+		else if (player.velocity.y < 0) {
+			player.velocity += Vector3.up * Physics.gravity.y * (gravityIncreaser - 1) * Time.deltaTime;	
+		}
+		//Shoot
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			
+		}
+	}
+
+	bool IsGrounded(){
+		return Physics.Raycast (transform.position, -Vector3.up, collider.bounds.extents.y + extraYCollide);
+	}
+
+	void Shoot(){
+		GameObject bullet = pool.Recycle ();
+		Rigidbody bulletRB = bullet.GetComponent<Rigidbody> ();
+		bulletRB.AddForce (bulletSpawnPoint.forward,ForceMode.Impulse);
+	}
+}
---
+++ Assets/Scripts/Pool.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool : MonoBehaviour {
+	public GameObject pooledObj;
+	public int amount;
+	private List<GameObject> pool;
+	void Start () {
+		GameObject Bucket = new GameObject (pooledObj.name + "_bucket");
+		for (int i = 0; i < amount; i++) {
+			GameObject aux = (GameObject)Instantiate (pooledObj, Vector3.zero, Quaternion.identity);
+			aux.SetActive (false);
+			aux.transform.parent = Bucket.transform;
+			pool.Add (aux);
+		}
+	}
+	public GameObject Recycle(){
+		foreach (GameObject o in pool) {
+			if (!o.activeInHierarchy) {
+				o.SetActive (true);
+				return  o;
+			} 
+		}
+		return null;
+	}
+}
