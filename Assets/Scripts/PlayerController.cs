03e8028a6192330d88d8fab141ba92f1ce09ec50
LAPTOP-Q6FQDGEF\chetobros <chetobros@gmail.com>
2018-06-05 14:54:34 -0500

    Dash added
    Dash added & gravity off... Animations included

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	[Header("Player properties")]
-	private float horizontal;
+	[SerializeField]
+	private GameObject playerModel;
+	//Horizontal move
 	public float speed;
+	public float dashSpeed;
+	private float horizontal;
+	[SerializeField]
+	private bool tapped = false;
+	public float tapDelay = 0.5f;
+	private int tapCount = 0;
+	[SerializeField]
+	private float currentTapDelay;
+	//Jump
 	public float jumpForce;
+	//Climb
 	public float climbDistance;
 	public float climbTime;
 	private float currentClimbTime;
-	[SerializeField]
 	private bool walled = false;
 	[Header("Extra player properties")]
-	public float extraYCollide;
 	public float extraXCollide;
+	public float extraYCollide;
+	[SerializeField]
+	private GameObject dashParticle;
 	[Header("Gravity modifiers")]
 	public float jumpMultiplier;
 	public float gravityIncreaser;
@@ -56,7 +69,30 @@ public class PlayerController : MonoBehaviour {
 
 		//Horizontal move
 		if (horizontal != 0) {
-			player.velocity = new Vector3 (speed * horizontal * deltaTime, player.velocity.y, 0f);
+			if (Input.GetAxisRaw ("Horizontal") == 0) {
+				tapCount++;
+			}
+			if (tapped && currentTapDelay > 0 && tapCount>1) {
+				player.velocity = new Vector3 (dashSpeed * horizontal * deltaTime, player.velocity.y, 0f);	
+				currentTapDelay = tapDelay;
+			} else {
+				player.velocity = new Vector3 (speed * horizontal * deltaTime, player.velocity.y, 0f);
+			}
+			if (!tapped) {
+				tapped = true;
+				currentTapDelay = tapDelay;
+			}
+			if (horizontal > 0)
+				playerModel.transform.rotation = Quaternion.Euler (0, 0, 0);
+			else if (horizontal < 0)
+				playerModel.transform.rotation = Quaternion.Euler (0, 180.0f, 0);
+		}
+		if (tapped) {
+			currentTapDelay-= Time.deltaTime;
+		}
+		if (currentTapDelay <= 0) {
+			tapped = false;
+			tapCount = 0;
 		}
 
 		//Jump
@@ -69,8 +105,8 @@ public class PlayerController : MonoBehaviour {
 		if (player.velocity.y > 0 && (!Input.GetButton("Jump") && !Input.GetKey(KeyCode.UpArrow))) {
 			player.velocity += Vector3.up * Physics.gravity.y * (jumpMultiplier - 1) * deltaTime;
 		}
-		else if (player.velocity.y < 0 || walled) {
-			player.velocity += Vector3.up * Physics.gravity.y * (gravityIncreaser - 1) * deltaTime;	
+		else if (player.velocity.y < 0) {
+			//player.velocity += Vector3.up * Physics.gravity.y * (gravityIncreaser - 1) * deltaTime;	
 		}
 
 		//Wall jump
@@ -83,12 +119,16 @@ public class PlayerController : MonoBehaviour {
 
 		//Climb
 		if (walled && vertical) {
-			transform.position = new Vector3 (transform.position.x, transform.position.y + (climbDistance * deltaTime), transform.position.z);
+			//player.velocity = Vector3.zero;
+			player.MovePosition (transform.position + Vector3.up * climbDistance * deltaTime);
 			currentClimbTime += deltaTime;
 			if (currentClimbTime > climbTime) {
 				currentClimbTime = 0;
-				transform.position = new Vector3 (transform.position.x, transform.position.y + (climbDistance * deltaTime), transform.position.z);
+				player.MovePosition (transform.position + Vector3.up * climbDistance * deltaTime);
 			}
+		} else if (walled) {
+			//Salto hacia afuera + gravedad añadida
+
 		} else if (!walled) {
 			currentClimbTime = climbTime;
 		}
